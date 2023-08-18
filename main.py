ecac1b8dc6f53e26f9705eb95f2df2faa186403c
Ritwikgotbugs <shritwik04@gmail.com>
2023-08-18 21:00:49 +0530

    Improved performance

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 # import the necessary packages
 from scipy.spatial import distance as dist
-from imutils.video import FileVideoStream
-from imutils.video import VideoStream
 from imutils import face_utils
-import numpy as np
-import imutils
-import time
+import wget
 import dlib
 import cv2
 import os
 
+
+def download_facelandmark_model():
+	if(os.path.exists("model/shape_predictor_68_face_landmarks.dat")):
+		print("Yes")
+	else:
+		try:
+			url='https://github.com/italojs/facial-landmarks-recognition/raw/master/shape_predictor_68_face_landmarks.dat'
+			wget.download(url)
+			os.rename("shape_predictor_68_face_landmarks.dat", "model/shape_predictor_68_face_landmarks.dat")
+		except:
+			print("Please connect to the internet...")
+			exit()
+
+
+download_facelandmark_model()
+
 def eye_aspect_ratio(eye):
-    # compute the euclidean distances between the two sets of
-    # vertical eye landmarks (x, y)-coordinates
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
 
-    # compute the euclidean distance between the horizontal
-    # eye landmark (x, y)-coordinates
-    C = dist.euclidean(eye[0], eye[3])
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
 
-    # compute the eye aspect ratio
-    ear = (A + B) / (2.0 * C)
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
 
-    # return the eye aspect ratio
-    return ear
+	# return the eye aspect ratio
+	return ear
+
 
 # define two constants, one for the eye aspect ratio to indicate
 # blink and then a second constant for the number of consecutive
@@ -40,64 +53,80 @@ TOTAL = 0
 # the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
-
-# Load the local shape predictor file
-shape_predictor_path = "model/shape_predictor_68_face_landmarks.dat"
-if not os.path.exists(shape_predictor_path):
-    print("Please place shape_predictor_68_face_landmarks.dat in the 'model' folder.")
-    exit()
-predictor = dlib.shape_predictor(shape_predictor_path)
+predictor = dlib.shape_predictor("model/shape_predictor_68_face_landmarks.dat")
 
 # grab the indexes of the facial landmarks for the left and
 # right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+
 cap = cv2.VideoCapture(0)
 
 # loop over frames from the video stream
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # detect faces in the grayscale frame
-    rects = detector(gray, 0)
+	ret,frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # loop over the face detections
-    for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+	# detect faces in the grayscale frame
+	rects = detector(gray, 0)
 
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
+	# loop over the face detections
+	for rect in rects:
+		# determine the facial landmarks for the face region, then
+		# convert the facial landmark (x, y)-coordinates to a NumPy
+		# array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
 
-        # average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
+		# extract the left and right eye coordinates, then use the
+		# coordinates to compute the eye aspect ratio for both eyes
+		leftEye = shape[lStart:lEnd]
+		rightEye = shape[rStart:rEnd]
+		leftEAR = eye_aspect_ratio(leftEye)
+		rightEAR = eye_aspect_ratio(rightEye)
 
-        # ... (rest of the code remains unchanged)
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
 
-        # draw the total number of blinks on the frame along with
-        # the computed eye aspect ratio for the frame
-        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-    # show the frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+		# check to see if the eye aspect ratio is below the blink
+		# threshold, and if so, increment the blink frame counter
+		if ear < EYE_AR_THRESH:
+			COUNTER += 1
 
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+		# otherwise, the eye aspect ratio is not below the blink
+		# threshold
+		else:
+			# if the eyes were closed for a sufficient number of
+			# then increment the total number of blinks
+			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				TOTAL += 1
+
+			# reset the eye frame counter
+			COUNTER = 0
+
+		# draw the total number of blinks on the frame along with
+		# the computed eye aspect ratio for the frame
+		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+	# show the frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
-cap.release()
